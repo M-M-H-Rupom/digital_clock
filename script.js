9dc0c9d5ce576b9b768e691667df2e14a19f4b89
Rupom <mohrajulrupom@gmail.com>
2024-05-28 14:31:05 +0600

    woking with digital clock

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+$(document).ready(function() {
+    setInterval(function() {
+        update_time();
+    }, 1000);
+});
+function update_time(){
+    let date = new Date();
+    let hour = date.getHours().toString().padStart(2, '0');
+    let minute = date.getMinutes().toString().padStart(2, '0');
+    let second = date.getSeconds().toString().padStart(2, '0');
+    let digit_object = {
+        'h1': hour[0],
+        'h2': hour[1],
+        'm1': minute[0],
+        'm2': minute[1],
+        's1': second[0],
+        's2': second[1]
+    };
+    let segments = [
+        'abcdef', 
+        'bc', 
+        'abdeg', 
+        'abcdg', 
+        'bcfg', 
+        'acdfg', 
+        'acdefg', 
+        'abc', 
+        'abcdefg', 
+        'abcdfg', 
+    ];
+    $('.segment').hide();
+    for (let key in digit_object) {
+        let date_digit = parseInt(digit_object[key]);
+        let single_segment_map = segments[date_digit];
+        let single_segment_map_arr = single_segment_map.split("");
+        for (let j = 0; j < single_segment_map_arr.length; j++) {
+            $('#' + key + '_' + single_segment_map_arr[j]).show(); 
+        }
+    }
+}
